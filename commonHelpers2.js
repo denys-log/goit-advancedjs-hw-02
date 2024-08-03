08eb2e80a5cbb2f29f9edd7a53f1b410f73e5db5
denys-utsekha <denysutsekha@gmail.com>
2024-08-03 12:34:28 +0000

    Deploying to gh-pages from @ denys-utsekha/goit-advancedjs-hw-02@b73e5514f3ef63233db5fce70a62a471586d44c8 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const a=document.querySelector(".form");a.addEventListener("submit",t=>{t.preventDefault();const o=new FormData(t.target),s=parseInt(o.get("delay")),r=o.get("state");new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(s):m(s)},s)}).then(e=>{i.success({message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight"})}),t.target.reset()});
+//# sourceMappingURL=commonHelpers2.js.map
